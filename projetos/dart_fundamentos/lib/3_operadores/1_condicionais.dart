2e89f05b05f5eb00fd279c77fb1300380d5764e7
caique-dourado <eng.caiquedourado@gmail.com>
2022-02-13 19:02:17 -0300

    aula03 - operadores condicionais (regras de negocio)

---
+++ projetos/dart_fundamentos/lib/3_operadores/1_condicionais.dart
@@ -0,0 +1,10 @@
+void main() {
+  //! Os operadores condocionais, resumem-se no uso do if else
+  final idade = 18;
+
+  if (idade >= 18) {
+    print('Pode tirar a habilitação');
+  } else {
+    print('Ainda não pode tirar a habilitação');
+  }
+}
