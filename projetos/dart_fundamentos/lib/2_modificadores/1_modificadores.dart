a40f2ffeb534f48fb93e39016031c712dbc85d7b
caique-dourado <eng.caiquedourado@gmail.com>
2022-02-13 19:01:18 -0300

    aula02 - modificadores final e const

---
+++ projetos/dart_fundamentos/lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,9 @@
+void main() {
+  //! final - a variável passa a ser imutável, isto é, depois de inicializada não pode mais ser alterada
+
+  final nomeCompletoFinal;
+  nomeCompletoFinal = 'Caique Dourado';
+//* -------------------------------------------------------------
+  //! const - tem a mesma característica de final, porém os dados devem ta prontos em tempo de compilação
+  const nomeCompletoConst = 'Caique Dourado';
+}
