a48b82abc4dfedbbe85f0ef6c38d117376c778f7
caique-dourado <eng.caiquedourado@gmail.com>
2022-02-13 19:04:52 -0300

    aula03 - operador ternarios, como usar

---
+++ projetos/dart_fundamentos/lib/3_operadores/4_ternarios.dart
@@ -0,0 +1,10 @@
+void main() {
+  //? É uma simplificação do if else.
+
+  //! (condicao)? faça algo : outra coisa;
+
+  final idade = 14;
+  final eMaiorIdade = (idade >= 18) ? true : false;
+
+  print(eMaiorIdade);
+}
