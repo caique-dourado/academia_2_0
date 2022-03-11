55e92c43a634aab0f9202935044cd8a8dac3027c
caique-dourado <eng.caiquedourado@gmail.com>
2022-03-10 21:08:41 -0300

    aprendendo imports

---
+++ projetos/dart_funcoes_colecoes/lib/6_imports/1_import.dart
@@ -0,0 +1,7 @@
+import 'somas/soma.dart'; //IMPORT RELATIVO
+import 'package:dart_funcoes_colecoes/6_imports/somas/soma.dart'; // IMPORT PACKAGE
+
+void main() {
+  print(somaInteiro(10, 52));
+  print(somaDouble(30.02, 99.98));
+}
---
+++ projetos/dart_funcoes_colecoes/lib/6_imports/somas/soma.dart
@@ -0,0 +1,5 @@
+somaInteiro(int numero1, int numero2) => numero1 + numero2;
+
+somaDouble(double numero1, double numero2) {
+  return numero1 + numero2;
+}
