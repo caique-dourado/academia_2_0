a7aedff0d7a98c24fb274521115b38e9e771f087
caique-dourado <eng.caiquedourado@gmail.com>
2022-03-31 20:25:40 -0300

    assigment operator

---
+++ projetos/dart_poo/lib/14_assigment_operator/assigment_operator.dart
@@ -0,0 +1,14 @@
+String? nome;
+
+void main() {
+//!Assigment operators
+// =    -=    /=    %=    >>=   ^=
+// +=   *=    ~/=   <<=   &=    |=
+
+//* Mais usados
+//+=  -=  ??=
+
+//Para fazer uma checagem de nulo pode-se usar o null or operator (??=) ao
+//inves do if()
+  print(nome ??= 'Caique Dourado');
+}
